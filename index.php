83b3a3ec2070847393c3838a8d1d62d82ea77a4a
monchoramon <31085401+monchoramon@users.noreply.github.com>
2021-10-07 19:47:17 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/main.css">
     <title>Snake 30-07-2021</title>
+    <link rel="stylesheet" href="assets/css/main.css"/>
 </head>
 <body>
     <div class="main">
